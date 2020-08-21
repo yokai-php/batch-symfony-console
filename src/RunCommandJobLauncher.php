5f7b3857d32aa2503706b65ef97be90f84a39aaa
Yann Eugoné <yeugone@prestaconcept.net>
2020-08-21 18:42:31 +0200

    Switch to monorepo

---
+++ src/RunCommandJobLauncher.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yokai\Batch\Bridge\Symfony\Console;
+
+use Yokai\Batch\BatchStatus;
+use Yokai\Batch\Factory\JobExecutionFactory;
+use Yokai\Batch\JobExecution;
+use Yokai\Batch\Launcher\JobLauncherInterface;
+use Yokai\Batch\Storage\JobExecutionStorageInterface;
+
+final class RunCommandJobLauncher implements JobLauncherInterface
+{
+    /**
+     * @var JobExecutionFactory
+     */
+    private $jobExecutionFactory;
+
+    /**
+     * @var CommandRunner
+     */
+    private $commandRunner;
+
+    /**
+     * @var string
+     */
+    private $logFilename;
+
+    /**
+     * @var JobExecutionStorageInterface
+     */
+    private $jobExecutionStorage;
+
+    public function __construct(
+        JobExecutionFactory $jobExecutionFactory,
+        CommandRunner $commandRunner,
+        JobExecutionStorageInterface $jobExecutionStorage,
+        string $logFilename = 'batch_execute.log'
+    ) {
+        $this->jobExecutionFactory = $jobExecutionFactory;
+        $this->logFilename = $logFilename;
+        $this->commandRunner = $commandRunner;
+        $this->jobExecutionStorage = $jobExecutionStorage;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function launch(string $name, array $configuration = []): JobExecution
+    {
+        $configuration['_id'] = $configuration['_id'] ?? uniqid();
+
+        $jobExecution = $this->jobExecutionFactory->create($name, $configuration);
+        $jobExecution->setStatus(BatchStatus::PENDING);
+        $this->jobExecutionStorage->store($jobExecution);
+
+        $this->commandRunner->runAsync(
+            'yokai:batch:run',
+            $this->logFilename,
+            [
+                'job' => $name,
+                'configuration' => json_encode($configuration),
+            ]
+        );
+
+        return $jobExecution;
+    }
+}
